65f625b47ef7c3b5e0348d0b314992a81384aef0
unknown <doutycharlesdiarra3@gmail.com>
2026-07-27 23:19:36 +0000

    Ajout des donnee a travers seeding (DbOtherSeeder)

---
+++ Migrations/20260727231652_AjoutDbOtherSeeder.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AfroEvent.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjoutDbOtherSeeder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
